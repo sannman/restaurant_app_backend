a7af41b4d49cb4f36edc0327937a8a32ef5d2e30
Sannman Prabhu <prabhusanman2@gmail.com>
2025-10-20 01:31:29 +0530

    FASTAPI SERVER IMPLEMENTATION

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import flask
 from dotenv import load_dotenv
 from supabase import create_client, Client
 
@@ -10,24 +11,29 @@ url: str = os.environ.get("SUPABASE_URL")
 anon_key: str = os.environ.get("SUPABASE_ANON_KEY") or os.environ.get("SUPABASE_KEY")
 
 
+app = flask.Flask(__name__)
 
 supabase: Client = create_client(url, anon_key)
 
-user_what = input("Buyer or Seller? ")
 
-if user_what == "Seller" or user_what == "S" or user_what == "s":
-    dish = input("Enter dish name: ")
-    price = int(input("Enter dish price: "))  
-    response = (
-        supabase.table("DISH")
-        .insert({"id": 5, "Dish": dish,"Price": price})
-        .execute()
-)
-
-if user_what == "Buyer" or user_what == "B" or user_what == "b":
+@app.route("/buy_dishes", methods=["GET"])
+def get_dishes():
     response = (
         supabase.table("DISH")
         .select("*")
         .execute()
     )
-print(response.data)
+    return flask.jsonify(response.data)
+
+
+@app.route("/add_dishes", methods=["POST"])
+# will get user input to add a new dish
+def add_dish():
+    data = flask.request.get_json()
+    dish = data.get("Dish")
+    price = data.get("Price")
+    response = supabase.table("DISH").insert({"id":7, "Dish": dish,"Price": price}).execute()
+    return flask.jsonify(response.data)
+
+if __name__ == "__main__":
+    app.run(debug=True)
