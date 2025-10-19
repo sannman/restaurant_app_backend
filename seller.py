0c71b1c7a4a112bfd2b4c60383f1262b932162a0
Sannman Prabhu <prabhusanman2@gmail.com>
2025-10-20 03:04:47 +0530

    Implemented auth doesnt work on postman idk why maybe i am dumb

--- seller.py
+++ seller.py
@@ -10,3 +10,33 @@ def add_dish():
     price = data.get("Price")
     response = supabase.table("DISH").insert({"Dish": dish, "Price": price}).execute()
     return jsonify(response.data)
+
+
+@seller_bp.route("/sellers/login", methods=["POST"])
+def seller_login():
+    data = request.get_json()
+    email = data.get()
+    password = data.get()
+    response = supabase.auth.sign_in_with_password(
+    {
+        "email": email,
+        "password": password,
+    }
+)
+    ...
+
+
+@seller_bp.route("/sellers/signup", methods=["POST"])
+def seller_signup():
+
+    data = request.get_json()
+    email = data.get("email")
+    password = data.get("password")
+    response = supabase.auth.sign_up(
+    {
+        "email": email,
+        "password": password,
+    }
+)
+    ...
+
