4c546155415fb7846708d7faa8cef0ce2136ef5f
Sannman Prabhu <prabhusanman2@gmail.com>
2025-10-20 17:49:23 +0530

    fix minnor things

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask_cors import CORS
 from buyer import buyer_bp
 from seller import seller_bp
 
 app = Flask(__name__)
+CORS(app)
 
 app.register_blueprint(buyer_bp, url_prefix="/buyer")
 app.register_blueprint(seller_bp, url_prefix="/seller")
