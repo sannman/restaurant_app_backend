71633f6e402f853a9c339614751f0108bb7bb261
Sannman Prabhu <playsnoob82@gmail.com>
2025-10-29 15:30:08 +0530

    new endpoint dishes to work with dishes

--- dishes.py
+++ dishes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify , request
 from secret import supabase 
-
+from seller import get_seller_by_id
 
 dishes_bp = Blueprint("dishes", __name__)
 
@@ -17,3 +17,26 @@ def search_dishes():
     print(response.data)
     return jsonify(response.data)
 
+@dishes_bp.route("/add/dishes", methods=["POST"])
+def add_dish():
+    data = request.get_json()
+    print("Received data:", data)
+    dish = data.get("dish")
+    price = data.get("price")
+    email = data.get("email")
+    seller = get_seller_by_id(email)
+    if not seller:
+        return jsonify({"error": "Seller not found."}), 404
+    if seller["role_id"] != 1:
+        return jsonify({"error": "Only sellers can add dishes."}), 403
+    if seller["role_id"] == 1:
+        dish_data = {
+            
+            "dish": dish,
+            "price": price,
+            "user_id": seller["id"],
+            "role_id": seller["role_id"]
+        }
+        print("Dish data " , dish_data)
+        supabase.table("dish").insert(dish_data).execute()
+        return jsonify(f"Dish added name : {dish} , price : {price}"), 201
